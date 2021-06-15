83d1f50436da85fc6384c4ec6700854873bf5790
Bharthi <bharthi31@mgail.co>
2021-06-15 16:27:02 +0530

    Fixed issue with resource name

--- ci_cd/adc_configs/resources.tf
+++ ci_cd/adc_configs/resources.tf
@@ -7,15 +7,15 @@ resource "citrixadc_csvserver" "demo_csvserver" {
   # lbvserverbinding = citrixadc_lbvserver.blueLB.name
 }
 
-resource "citrixadc_lbvserver" {
+resource "citrixadc_lbvserver"  "lbvs"{
   ipv46       = var.lb_ip
   name        = var.lbvs_name
   port        = 80
   servicetype = "HTTP"
 }
 
-resource "citrixadc_service" {
-    lbvserver = citrixadc_lbvserver.blueLB.name
+resource "citrixadc_service"  "service"{
+    lbvserver = citrixadc_lbvserver.lbvs.name
     name = var.backend_service_name
     ip = var.backend_service
     servicetype  = "HTTP"
@@ -25,17 +25,16 @@ resource "citrixadc_service" {
 
 
 #policy to based on that target lbvserver
-resource "citrixadc_cspolicy" {
+resource "citrixadc_cspolicy" "cspolicy"{
   csvserver       = citrixadc_csvserver.demo_csvserver.name
   # targetlbvserver = citrixadc_lbvserver.blueLB.name
   policyname      = var.cspolicy_name
-  action          = citrixadc_csaction.blue_csaction.name
   rule            = format("HTTP.REQ.HOSTNAME.SERVER.EQ(\"demo-bg.webapp.com\") && HTTP.REQ.URL.PATH.SET_TEXT_MODE(IGNORECASE).STARTSWITH(\"/\") && sys.random.mul(100).lt(%s)", var.traffic_split_percentage)
   priority        = var.priority
 
   # Any change in the following id set will force recreation of the cs policy
   forcenew_id_set = [
-    citrixadc_lbvserver.blueLB.id,
+    citrixadc_lbvserver.lbvs.id,
     citrixadc_csvserver.demo_csvserver.id,
   ]
 }

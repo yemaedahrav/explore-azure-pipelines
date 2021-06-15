3cf7bb2cf8128e6bc556f7110e2f1e99b1cc64f3
Bharthi <bharthi31@mgail.co>
2021-06-15 19:47:18 +0530

    testing

--- ci_cd/adc_configs/variables.tf
+++ ci_cd/adc_configs/variables.tf
@@ -1,28 +1,35 @@
 variable "traffic_split_percentage" {
   description = "Percentage of Traffic to be split"
   type        = number
+  default     = 100
 }
 variable "backend_service" {
   description = "The backend service IP"
   type        = string
+  default     = "20.0.0.7"
 }
 variable "lbvs_name" {
   description = "Name of LB Vserver"
   type = string
+  default = "lbvs"
 }
 variable "lb_ip" {
   description = "IP of LB Vserver"
   type = string
+  default = "1.1.1.1"
 }
 variable "cspolicy_name" {
   description = "Name of CS Policy"
   type = string
+  default = "cspolicy"
 }
 variable "backend_service_name" {
   description = "backend service"
   type = string
+  default = "service"
 }
 variable "priority" {
   description = "CS Policy Priority"
   type = number
+  default = 100
 }
